80419b96b8b14df0d401f71ca4346de541c6beaa
Aman Madaan <madaan.amanmadaan@gmail.com>
2014-04-19 22:59:15 +0530

    Improve table layout, add readme.

--- src/caching/ShowCachedActivity.java
+++ src/caching/ShowCachedActivity.java
@@ -3,6 +3,7 @@ package caching;
 import java.util.ArrayList;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.widget.TableLayout;
@@ -35,34 +36,42 @@ public class ShowCachedActivity extends Activity {
 	void populate() {
 		TranslationCachingHandler th = new TranslationCachingHandler(getApplicationContext());
 		ArrayList<CachedTranslation> allTrans = th.getAllTranslations();
-		for (int i = 0; i < allTrans.size(); i++) {
+		for (int i = 1; i < allTrans.size(); i++) {
 			TableRow tr = new TableRow(this);
+			TableLayout.LayoutParams trl = new TableLayout.LayoutParams(LayoutParams.WRAP_CONTENT,
+					LayoutParams.WRAP_CONTENT); 
+			trl.width = R.string._0dp;
+			trl.weight = R.string._1;
 			tr.setId(i);
-			tr.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT,
-					LayoutParams.WRAP_CONTENT));
+			tr.setLayoutParams(trl);
 			TextView english = new TextView(this);
 			english.setId(i + 10);
 			english.setPadding(20, 20, 20, 20);
 			//english.setBackgroundColor(color.LightGrey);
 			
-			english.setText(allTrans.get(i).getEnglish());
+			String englishTrans = allTrans.get(i).getEnglish();
+			String hindiTrans = allTrans.get(i).getHindi();
+			
+			english.setText(englishTrans + "- >" + hindiTrans);
 			
 
 			english.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT,
 					LayoutParams.WRAP_CONTENT));
-		
+			
+			tr.setBackgroundColor(Color.rgb((i * 10) % 256 + 100, (i * 10) % 256  + 100, (i * 10) % 256  + 100));
+			//tr.setBackgroundColor(Color.)
 			tr.addView(english);
-
+/*
 			TextView hindi = new TextView(this);
 			hindi.setId(i + 10);
-			hindi.setPadding(20, 20, 20, 20);
+			//hindi.setPadding(20, 20, 20, 20);
 			//hindi.setBackgroundColor(color.SlateBlue);
 			hindi.setText(allTrans.get(i).getHindi());
 			hindi.setTypeface(hindiTypeface);
 			hindi.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT,
 					LayoutParams.WRAP_CONTENT));
 			tr.addView(hindi);
-
+*/
 			// Add the TableRow to the TableLayout
 			tl.addView(tr, new TableLayout.LayoutParams(
 					LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
